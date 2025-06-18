4e55f2ad68e1d8fc04daef4cac1af6ec226273a4
khansabih <khansabih8@gmail.com>
2025-06-19 00:45:03 +0100

    index.ts file updated

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,3 @@
-// import express from 'express';
-// import cors from 'cors';
-// import dotenv from 'dotenv';
-
-// // Load environment variables
-// dotenv.config();
-
-// const app = express();
-// app.use(cors());
-// app.use(express.json());
-
-// app.get('/', (req, res) => {
-//   res.send('Hello from TypeScript backend!');
-// });
-
-// const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () => {
-//   console.log(`🚀 Server running on http://localhost:${PORT}`);
-// });
-
 import express from 'express';
 import studentRoutes from './routes/students';
 // import producerRoutes from './routes/producers';
@@ -36,5 +16,5 @@ app.use('/students', studentRoutes);
 // app.use('/admin', adminRoutes);
 
 app.listen(5000, () => {
-  console.log('🚀 Server running on http://localhost:5000');
+  console.log('Server running on http://localhost:5000');
 });
